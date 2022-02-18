98baff93a9fd6c7bf22c941e17c0b97b2cc3c417
arandjelovicstefan <87954129+arandjelovicstefan@users.noreply.github.com>
2022-02-19 00:25:28 +0100

    reactive-forms

--- angular-template-driven-forms/src/app/app.component.ts
+++ angular-template-driven-forms/src/app/app.component.ts
@@ -8,13 +8,43 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('form') signupForm!: NgForm;
+  @ViewChild('loginForm') loginForm!: NgForm;
   // drugi nacin za pristup formi, takodje i nacin koji nam dozvoljava da imamo pristup formi ranije a ne samo na submit !!
-
   defaultQuestion: string = 'Pet';
   answer: string = '';
+  genders: string[] = ['male', 'female'];
+  subscriptions: string[] = ['Basic', 'Advanced', 'Pro'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: '',
+  };
+  submitted: boolean = false;
+
+  formSwitch: boolean = true;
 
   suggestUserName() {
-    const suggestedName = 'Superuser';
+    const suggestedName = 'Superuser'; // ovo moze da se resi na vise nacina, jedan je two-way databinding, drugi da pristupimo direktno formi preko viewChild
+
+    // sa setValue mozemo isto da pristupimo, ali je problem sto moramo bas onako kakva je forma da dodamo objekat i sve vrednosti tom prilikom menjamo
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: '',
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male',
+    // });
+
+    //bolji nacin je sa patchValue, mozemo da setujemo samo jedan value bez potrebe da se resetuju druge stvari
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName,
+      },
+    });
   }
 
   // onSubmit(form: NgForm) {
@@ -23,6 +53,22 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset(); // resetovanje forme, moguce je i sa setValue ako hocemo da resetujemo na neki specifican value
+  }
+
+  onFormSwitch() {
+    this.formSwitch = !this.formSwitch;
+  }
+
+  onSubmitLogin() {
+    console.log(this.loginForm.form);
+    // this.loginForm.reset();
   }
 }
